aa1700ca840fdba64641b1134a345d51d1377c97
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-20 12:56:13 -0400

    Reworked Header ordering and added the cart context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,27 @@ import React, { useContext } from 'react'
 import '../styles/Header.css'
 
 const Header = () => {
-    const [cartGames] = useContext(CartContext)
+    const cartGames = useContext(CartContext)
 
     return (
         <nav className='cart-nav'>
             <div className='left-nav'>
-                <Link className='nav-link' to='/'>STORE</Link>
-                <Link className='nav-link' to='/library'>LIBRARY</Link>
+                <Link className='nav-link' to='/'>Home</Link>
+            </div>
+
+            <div className='middle-nav'>
+                <input className='nav-search' type='search' placeholder='Search games...'/>
             </div>
 
             <div className='right-nav'>
+                <Link className='nav-link' to='/library'>LIBRARY</Link>
                 <Link className='nav-link' to='/cart'>
                     CART
-                    {/* {cartGames.length > 0 && (
+                    {cartGames.length > 0 && (
                         <div className='cart-count'>
                             {cartGames.length}
                         </div>
-                    )} */}
+                    )}
                 </Link>
             </div>
         </nav>
