552673898e92ce43ef75351de3d6287320c8f2da
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-21 00:35:32 -0400

    Added useLocation to track the state prop of the linked game

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { useLocation } from 'react-router-dom'
 import Header from '../components/Header'
+import React from 'react'
 
-const Game = () => {
+const Game = (props) => {
+    //Gets the game data passed from the link
+    const location = useLocation()
+    const { game } = location.state
+
     return (
         <>
             <Header />
