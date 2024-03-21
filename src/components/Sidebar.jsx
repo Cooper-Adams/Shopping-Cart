745ad440ed6befdc8501648a638dbb592794711a
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-21 00:32:38 -0400

    Added Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+import '../styles/Sidebar.css'
+
+const Sidebar = () => {
+    return (
+        <div className='sc-sidebar'>
+            This is filler content for now. Lorem ipsum solum dolor
+        </div>
+    )
+}
+
+export default Sidebar
