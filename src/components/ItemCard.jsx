f68391914c7271ea0833837736ee2b5eca3f65e0
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-03-22 01:09:25 -0400

    Changed the Link to only wrap the title rather than the whole card

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -12,27 +12,27 @@ const ItemCard = (item) => {
         formattedDate = date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear()
     }
 
-    return (
-        <Link to={'game/' + item.game.slug} state={{game: item.game}}>
-            <div className='item-card'>
-                <img src={item.game.background_image} alt={item.game.name} />
+    return (        
+        <div className='item-card'>
+            <img src={item.game.background_image} alt={item.game.name} />
 
-                <div className='bottom-card'>
-                    <div className="bc-top">
+            <div className='bottom-card'>
+                <div className='bc-top'>
+                    <Link to={'game/' + item.game.slug} state={{game: item.game}}>
                         <h2 className='game-title'>{item.game.name}</h2>
+                    </Link>
 
-                        <h4 style={{display: item.game.rating_top === 0 ? 'none' : 'block'}} className={'game-rating ' + (item.game.rating / item.game.rating_top > .7 ? 'good' : item.game.rating / item.game.rating_top > .4 ? 'average' : item.game.rating > 0 ? 'bad' : '')}>{item.game.rating} / {item.game.rating_top}</h4>
-                    </div>
+                    <h4 style={{display: item.game.rating_top === 0 ? 'none' : 'block'}} className={'game-rating ' + (item.game.rating / item.game.rating_top > .7 ? 'good' : item.game.rating / item.game.rating_top > .4 ? 'average' : item.game.rating > 0 ? 'bad' : '')}>{item.game.rating} / {item.game.rating_top}</h4>
+                </div>
 
-                    <div className='card-rd'>
-                        <h4 className='release-date'>{todaysDate < date ? 'Releasing on' : 'Released'}: {formattedDate}</h4>
+                <div className='card-rd'>
+                    <h4 className='release-date'>{todaysDate < date ? 'Releasing on' : 'Released'}: {formattedDate}</h4>
 
-                        {/* Once library is set, add logic to display a 'You own this game' message if it is in the library already */}
-                        <h4 className='default-price'>$69.99</h4>
-                    </div>
+                    {/* Once library is set, add logic to display a 'You own this game' message if it is in the library already */}
+                    <h4 className='default-price'>$69.99</h4>
                 </div>
             </div>
-        </Link>
+        </div>
     )
 }
 
