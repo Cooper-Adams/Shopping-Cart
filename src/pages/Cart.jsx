a4b85ed7359931d230c6def722c0e6ef99eef211
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-04-17 15:09:43 -0400

    Added some conditional logic to the cart, but the loading needs fixing

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,16 +1,41 @@
-import { CCProvider } from '../contexts/CartContext'
+import { CartContext } from '../contexts/CartContext'
 import CartItemCard from '../components/CartItemCard'
 import Header from '../components/Header'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import '../styles/Cart.css'
 
 const Cart = () => {
+    const { cart } = useContext(CartContext)
+    const [loading, setLoading] = useState(false)
+
     return (
         <>
             <Header />
 
-            <div className='cart-cont'>
-                <h1>THIS IS WHERE THE GAMES YOU ADD GO</h1>
-            </div>
+            {/* {loading && cart.length != 0 && ( <div className='lds-dual-ring'></div> )} */}
+
+            {!loading && (
+                <>
+                    <h1 className='cart-title'>YOUR SHOPPING CART</h1>
+
+                    <div className='cart-cont'>
+                        {cart.length == 0 && (
+                            <h2 className='cart-empty'>Your cart is empty!</h2>
+                        )}
+
+                        {cart.length != 0 && (
+                            cart.map((game) => {
+                                return (
+                                    <CartItemCard 
+                                        game = {game}
+                                        setLoading = {setLoading}
+                                    />
+                                )
+                            })
+                        )}
+                    </div>
+                </>
+            )}
         </>
     )
 }
