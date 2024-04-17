97ed19976bb3d49ca9edf11d97209dfbe61d432e
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-04-17 15:11:40 -0400

    Removed unnecessary pieces from the cart context

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -5,23 +5,9 @@ export const CartContext = createContext([])
 export const CCProvider = (props) => {
     const [cart, setCart] = useState([])
 
-    const addToCart = (gameID) => {
-        setCart((prev) => ({ ...prev, [gameID]: prev[gameID] + 1 }))
-    }
-
-    const removeFromCart = (gameID) => {
-        setCart((prev) => ({ ...prev, [gameID]: prev[gameID] - 1 }))
-    }
-
-    const changeCount = (newAmount, gameID) => {
-        setCart((prev) => ({ ...prev, [gameID]: newAmount }))
-    }
-
     const ccValue = {
         cart,
-        addToCart,
-        removeFromCart,
-        changeCount
+        setCart,
     }
 
     return (
