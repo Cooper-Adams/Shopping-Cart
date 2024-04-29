870e68aeebcb550af6c4682fd4e4ba87ccd0a664
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-04-29 00:23:20 -0400

    Added the rest of the components to the cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,34 +5,49 @@ import React, { useContext, useState } from 'react'
 import '../styles/Cart.css'
 
 const Cart = () => {
-    const { cart } = useContext(CartContext)
+    const { cart, setCart } = useContext(CartContext)
     const [loading, setLoading] = useState(false)
+
+    const removeAll = () => { setCart([]) }
 
     return (
         <>
             <Header />
 
-            {/* {loading && cart.length != 0 && ( <div className='lds-dual-ring'></div> )} */}
+            {loading && cart.length != 0 && ( <div className='lds-dual-ring'></div> )}
 
             {!loading && (
                 <>
-                    <h1 className='cart-title'>YOUR SHOPPING CART</h1>
-
                     <div className='cart-cont'>
-                        {cart.length == 0 && (
-                            <h2 className='cart-empty'>Your cart is empty!</h2>
-                        )}
+                        <div className='ic-cont'>
+                            <h1 className='cart-title'>YOUR SHOPPING CART</h1>
 
-                        {cart.length != 0 && (
-                            cart.map((game) => {
-                                return (
-                                    <CartItemCard 
-                                        game = {game}
-                                        setLoading = {setLoading}
-                                    />
-                                )
-                            })
-                        )}
+                            {cart.length != 0 && (
+                                cart.map((game) => {
+                                    return (
+                                        <CartItemCard 
+                                            game = {game}
+                                            setLoading = {setLoading}
+                                        />
+                                    )
+                                })
+                            )}
+
+                            <div className='purchase-div'>
+                                <div className='pd-top'>
+                                    <span>Estimated Total:</span>
+
+                                    <span>${(69.99 * cart.length)}</span>
+                                </div>
+
+                                
+                                <div className='pd-btns'>
+                                    <button className='pd-rm' onClick={removeAll}>Remove all</button>
+                                    <button className='pd-btn'>Purchase for myself</button>
+                                    <button className='pd-btn'>Purchase as a gift</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </>
             )}
