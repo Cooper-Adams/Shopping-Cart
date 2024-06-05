7fcc7aee2d5ed9b7e7d13585c5d2bce9783c00dc
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-06-05 18:18:00 -0400

    Updated the selects in the bar as the customs were having focus problems

--- src/components/ResultBar.jsx
+++ src/components/ResultBar.jsx
@@ -2,224 +2,64 @@ import '../styles/ResultBar.css'
 import React, { useState } from 'react'
 
 const ResultBar = () => {
-    const [searchStatement, setSearchStatement] = useState('Showing games released this month')
+    const [searchStatement, setSearchStatement] = useState('Showing most popular games')
 
-    const toggleDropdown = (e) => { e.target.classList.toggle('is-active') }
-
-    const toggleSP = (e) => { 
-        e.stopPropagation()
-        e.target.classList.toggle('selected')
+    const submit = (e) => {
+        console.log(e)
     }
 
     return (
         <div className='shop-resultsbar'>
-            <h3 className='srb-statement'>{searchStatement}</h3>
+            <h4 className='srb-statement'>{searchStatement}</h4>    
 
             <form className='srb-form' action=''>
-                <div className='scs-def-tags' onClick={toggleDropdown}>
-                    Narrow by tag
-                    <ul onClick={toggleSP} className='scs-cdl'>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={45} name='tag' />
-                                2D
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={69} name='tag' />
-                                Action-Adventure
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={18} name='tag' />
-                                Co-op
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={8} name='tag' />
-                                First-Person
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={30} name='tag' />
-                                FPS
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={40836} name='tag' />
-                                Full Controller Support
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={16} name='tag' />
-                                Horror
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={7} name='tag' />
-                                Multiplayer
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={36} name='tag' />
-                                Open-World
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={24} name='tag' />
-                                RPG
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={32} name='tag' />
-                                Sci-Fi
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={31} name='tag' />
-                                Singleplayer
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={118} name='tag' />
-                                Story Rich
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={1} name='tag' />
-                                Survival
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={149} name='tag' />
-                                Third-Person
-                            </label>
-                        </li>
-                    </ul>
+                <div className='select-wrapper'>
+                    <select className='srb-ordering' name='tags' id='tag-select' onChange={submit} defaultValue={''}>
+                        <option style={{display: 'none'}} value='' disabled>Tags</option>
+                        <option className='srb-option' value='45'>2D</option>
+                        <option className='srb-option' value='69'>Action-Adventure</option>
+                        <option className='srb-option' value='18'>Co-Op</option>
+                        <option className='srb-option' value='8'>First-Person</option>
+                        <option className='srb-option' value='40836'>Full Controller Support</option>
+                        <option className='srb-option' value='16'>Horror</option>
+                        <option className='srb-option' value='7'>Multiplayer</option>
+                        <option className='srb-option' value='36'>Open-World</option>
+                        <option className='srb-option' value='24'>RPG</option>
+                        <option className='srb-option' value='32'>Sci-Fi</option>
+                        <option className='srb-option' value='31'>Singleplayer</option>
+                        <option className='srb-option' value='118'>Story Rich</option>
+                        <option className='srb-option' value='1'>Survival</option>
+                        <option className='srb-option' value='149'>Third-Person</option>
+                    </select>
                 </div>
 
-                <div className='scs-def-tags' onClick={toggleDropdown}>
-                    Narrow by genre
-                    <ul onClick={toggleSP} className='scs-cdl'>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={45} name='tag' />
-                                2D
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={69} name='tag' />
-                                Action-Adventure
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={18} name='tag' />
-                                Co-op
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={8} name='tag' />
-                                First-Person
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={30} name='tag' />
-                                FPS
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={40836} name='tag' />
-                                Full Controller Support
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={16} name='tag' />
-                                Horror
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={7} name='tag' />
-                                Multiplayer
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={36} name='tag' />
-                                Open-World
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={24} name='tag' />
-                                RPG
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={32} name='tag' />
-                                Sci-Fi
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={31} name='tag' />
-                                Singleplayer
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={118} name='tag' />
-                                Story Rich
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={1} name='tag' />
-                                Survival
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input type='checkbox' value={149} name='tag' />
-                                Third-Person
-                            </label>
-                        </li>
-                    </ul>
+                <div className='select-wrapper'>
+                    <select className='srb-ordering' name='genre' id='genre-select' defaultValue={''}>
+                        <option style={{display: 'none'}} value='' disabled>Genre</option>
+                        <option className='srb-option' value='added'>Relevance</option>
+                        <option className='srb-option' value='released'>Release Date</option>
+                        <option className='srb-option' value='name'>Name</option>
+                        <option className='srb-option' value='rating'>User Rating</option>
+                        <option className='srb-option' value='metacritic'>Metacritic Score</option>
+                    </select>
                 </div>
 
-                <select className='srb-ordering' name='ordering' id='ordering-select'>
-                    <option className='srb-option' value='added'>Relevance</option>
-                    <option className='srb-option' value='released'>Release Date</option>
-                    <option className='srb-option' value='name'>Name</option>
-                    <option className='srb-option' value='rating'>User Rating</option>
-                    <option className='srb-option' value='metacritic'>Metacritic Score</option>
-                </select>
+                <div className='select-wrapper'>
+                    <select className='srb-ordering' name='ordering' id='ordering-select'>
+                        <option className='srb-option' value='added'>Relevance</option>
+                        <option className='srb-option' value='released'>Release Date</option>
+                        <option className='srb-option' value='name'>Name</option>
+                        <option className='srb-option' value='rating'>User Rating</option>
+                        <option className='srb-option' value='metacritic'>Metacritic Score</option>
+                    </select>
+                </div>
 
-                <select className='srb-ordering' name='flip' id='ordering-flip'>
-                    <option className='srb-option' value=''>Descending</option>
-                    <option className='srb-option' value='-'>Ascending</option>
-                </select>
+                <div className='select-wrapper'>
+                    <select className='srb-ordering' name='flip' id='ordering-flip'>
+                        <option className='srb-option' value=''>Descending</option>
+                        <option className='srb-option' value='-'>Ascending</option>
+                    </select>
+                </div>
             </form>
         </div>
     )
