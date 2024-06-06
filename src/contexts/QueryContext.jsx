71b47ec8ebc877fb6b7765564ccc5bbf6165cdfb
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-06-06 00:40:34 -0400

    Fixed the incorrect states

--- src/contexts/QueryContext.jsx
+++ src/contexts/QueryContext.jsx
@@ -3,12 +3,12 @@ import React, { createContext, useState } from 'react'
 export const QueryContext = createContext([])
 
 export const QCProvider = (props) => {
-    const [order, setOrder] = useState('added')
+    const [order, setOrder] = useState('ordering=-')
     const [page, setPage] = useState(1)
     const [pageSize, setPageSize] = useState('page_size=15')
     const [queryGenre, setQueryGenre] = useState('')
     const [queryTag, setQueryTag] = useState('')
-    const [sort, setSort] = useState('ordering=')
+    const [sort, setSort] = useState('added')
 
     const ccValue = {
         order,
