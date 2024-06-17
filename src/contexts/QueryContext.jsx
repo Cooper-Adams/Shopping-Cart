f39e4c4e60303b6b271236e1f009324faa109b8b
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-06-16 23:50:00 -0400

    Added the dlc/addons exclusion to the query context

--- src/contexts/QueryContext.jsx
+++ src/contexts/QueryContext.jsx
@@ -3,16 +3,19 @@ import React, { createContext, useState } from 'react'
 export const QueryContext = createContext([])
 
 export const QCProvider = (props) => {
+    const [additions, setAdditions] = useState(false)
     const [order, setOrder] = useState('ordering=-')
     const [page, setPage] = useState(1)
     const [pageSize, setPageSize] = useState('page_size=15')
     const [queryGenre, setQueryGenre] = useState('')
     const [queryTag, setQueryTag] = useState('')
     const [sort, setSort] = useState('added')
-
-    const [query, setQuery] = useState('https://api.rawg.io/api/games?key=hellorobots&stores=1&page=' + page + '&' + pageSize + '&' + order + sort)
+    
+    const [query, setQuery] = useState('https://api.rawg.io/api/games?key=hellorobots&stores=1&page=' + page + '&' + pageSize + '&' + order + sort + '&exclude_additions=' + additions)
     
     const ccValue = {
+        additions,
+        setAdditions,
         order,
         setOrder,
         page,
