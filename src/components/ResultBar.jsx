3dfb26dc60a5221ac18346e86d5debe140e94f93
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-06-19 19:32:58 -0400

    Reordered the sections for readability as a vertical component

--- src/components/ResultBar.jsx
+++ src/components/ResultBar.jsx
@@ -2,7 +2,7 @@ import '../styles/ResultBar.css'
 import { QueryContext } from '../contexts/QueryContext'
 import React, { useContext } from 'react'
 
-const ResultBar = () => {
+const ResultBar = (props) => {
     const { additions, order, sort, queryTag, queryGenre, setAdditions, setOrder, setPage, setSearch, setSort, setQueryGenre, setQueryTag } = useContext(QueryContext)
 
     const clearQuery = (e) => {
@@ -31,74 +31,167 @@ const ResultBar = () => {
 
     return (
         <div className='shop-resultsbar'>
-            <form className='srb-form' action=''>
-                <label className='check-container' htmlFor='exclude'>Exclude Add-ons
-                    <input type='checkbox' id='exclude' name='exclude' onChange={updateQuery} value={additions}/>
+            <div className='srb-top'>
+                <h3 className='srb-top-number'>{(props.number == null ? 'Loading results...' : props.number + ' results')}</h3>
+                <h3 className='clear-btn' onClick={clearQuery}>Clear</h3>
+            </div>
+
+            <label className='check-container exclude' htmlFor='exclude'>Exclude Add-Ons
+                <input type='checkbox' id='exclude' name='exclude' onChange={updateQuery} value={additions}/>
+                <span className='checkmark'></span>
+            </label>
+
+            <form className='srb-genres' action=''>
+                <h3 className='srb-title'>Genres</h3>
+
+                <label className='check-container' htmlFor='action'>Action
+                    <input type='checkbox' name='action' value={4}/>
                     <span className='checkmark'></span>
                 </label>
 
-                <div className='select-wrapper'>
-                    <select className='srb-ordering' name='tags' id='tag' onChange={updateQuery} value={queryTag}>
-                        <option style={{display: 'none'}} value='' disabled>Tags</option>
-                        <option className='srb-option' value='45'>2D</option>
-                        <option className='srb-option' value='69'>Action-Adventure</option>
-                        <option className='srb-option' value='18'>Co-Op</option>
-                        <option className='srb-option' value='8'>First-Person</option>
-                        <option className='srb-option' value='40836'>Full Controller Support</option>
-                        <option className='srb-option' value='16'>Horror</option>
-                        <option className='srb-option' value='7'>Multiplayer</option>
-                        <option className='srb-option' value='36'>Open-World</option>
-                        <option className='srb-option' value='24'>RPG</option>
-                        <option className='srb-option' value='32'>Sci-Fi</option>
-                        <option className='srb-option' value='31'>Singleplayer</option>
-                        <option className='srb-option' value='118'>Story Rich</option>
-                        <option className='srb-option' value='1'>Survival</option>
-                        <option className='srb-option' value='149'>Third-Person</option>
-                    </select>
-                </div>
+                <label className='check-container' htmlFor='adventure'>Adventure
+                    <input type='checkbox' name='adventure' value={3}/>
+                    <span className='checkmark'></span>
+                </label>
 
-                <div className='select-wrapper'>
-                    <select className='srb-ordering' name='genre' id='genre' onChange={updateQuery} value={queryGenre}>
-                        <option style={{display: 'none'}} value='' disabled>Genre</option>
-                        <option className='srb-option' value='4'>Action</option>
-                        <option className='srb-option' value='3'>Adventure</option>
-                        <option className='srb-option' value='11'>Arcade</option>
-                        <option className='srb-option' value='28'>Board Games</option>
-                        <option className='srb-option' value='17'>Card</option>
-                        <option className='srb-option' value='40'>Casual</option>
-                        <option className='srb-option' value='34'>Educational</option>
-                        <option className='srb-option' value='19'>Family</option>
-                        <option className='srb-option' value='6'>Fighting</option>
-                        <option className='srb-option' value='51'>Indie</option>
-                        <option className='srb-option' value='83'>Platformer</option>
-                        <option className='srb-option' value='7'>Puzzle</option>
-                        <option className='srb-option' value='1'>Racing</option>
-                        <option className='srb-option' value='5'>RPG</option>
-                        <option className='srb-option' value='2'>Shooter</option>
-                        <option className='srb-option' value='14'>Simulation</option>
-                        <option className='srb-option' value='15'>Sports</option>
-                        <option className='srb-option' value='10'>Strategy</option>
-                    </select>
-                </div>
+                <label className='check-container' htmlFor='arcade'>Arcade
+                    <input type='checkbox' name='arcade' value={11}/>
+                    <span className='checkmark'></span>
+                </label>
 
-                <div className='select-wrapper'>
-                    <select className='srb-ordering' name='ordering' id='sort' onChange={updateQuery} value={sort}>
-                        <option className='srb-option' value='added'>Popularity</option>
-                        <option className='srb-option' value='released'>Release Date</option>
-                        <option className='srb-option' value='name'>Name</option>
-                        <option className='srb-option' value='rating'>User Rating</option>
-                        <option className='srb-option' value='metacritic'>Metacritic Score</option>
-                    </select>
-                </div>
+                <label className='check-container' htmlFor='casual'>Casual
+                    <input type='checkbox' name='casual' value={40}/>
+                    <span className='checkmark'></span>
+                </label>
 
-                <div className='select-wrapper'>
-                    <select className='srb-ordering' name='flip' id='order' onChange={updateQuery} value={order}>
-                        <option className='srb-option' value='ordering=-'>Descending</option>
-                        <option className='srb-option' value='ordering='>Ascending</option>
-                    </select>
-                </div>
+                <label className='check-container' htmlFor='family'>Family
+                    <input type='checkbox' name='family' value={19}/>
+                    <span className='checkmark'></span>
+                </label>
 
-                <button className='search-btn' type='button' onClick={clearQuery}>Reset</button>
+                <label className='check-container' htmlFor='fighting'>Fighting
+                    <input type='checkbox' name='fighting' value={6}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='indie'>Indie
+                    <input type='checkbox' name='indie' value={51}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='platformer'>Platformer
+                    <input type='checkbox' name='platformer' value={83}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='puzzle'>Puzzle
+                    <input type='checkbox' name='puzzle' value={7}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='racing'>Racing
+                    <input type='checkbox' name='racing' value={1}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='rpg'>RPG
+                    <input type='checkbox' name='rpg' value={5}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='shooter'>Shooter
+                    <input type='checkbox' name='shooter' value={2}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='simulation'>Simulation
+                    <input type='checkbox' name='simulation' value={14}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='sports'>Sports
+                    <input type='checkbox' name='sports' value={15}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='strategy'>Strategy
+                    <input type='checkbox' name='strategy' value={10}/>
+                    <span className='checkmark'></span>
+                </label>
+            </form>
+
+            <form className='srb-tags' action=''>
+                <h3 className='srb-title'>Tags</h3>
+
+                <label className='check-container' htmlFor='2d'>2D
+                    <input type='checkbox' name='2d' value={45}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='actionadventure'>Action-Adventure
+                    <input type='checkbox' name='actionadventure' value={69}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='coop'>Co-Op
+                    <input type='checkbox' name='coop' value={18}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='firstperson'>First-Person
+                    <input type='checkbox' name='firstperson' value={8}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='fullcontrollersupport'>Full Controller Support
+                    <input type='checkbox' name='fullcontrollersupport' value={40836}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='horror'>Horror
+                    <input type='checkbox' name='horror' value={16}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='multiplayer'>Multiplayer
+                    <input type='checkbox' name='multiplayer' value={7}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='openworld'>Open-World
+                    <input type='checkbox' name='openworld' value={36}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='rpg'>RPG
+                    <input type='checkbox' name='rpg' value={24}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='scifi'>Sci-Fi
+                    <input type='checkbox' name='scifi' value={32}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='singleplayer'>Singleplayer
+                    <input type='checkbox' name='singleplayer' value={31}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='storyrich'>Story Rich
+                    <input type='checkbox' name='storyrich' value={118}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='survival'>Survival
+                    <input type='checkbox' name='survival' value={1}/>
+                    <span className='checkmark'></span>
+                </label>
+
+                <label className='check-container' htmlFor='thirdperson'>Third-Person
+                    <input type='checkbox' name='thirdperson' value={149}/>
+                    <span className='checkmark'></span>
+                </label>
             </form>
         </div>
     )
