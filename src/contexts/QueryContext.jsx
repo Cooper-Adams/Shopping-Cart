0f9d05acfae9c6200a96d9bc395fce6cd037baa5
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-07-01 01:00:13 -0400

    Removed the order state from the query context as it was unused

--- src/contexts/QueryContext.jsx
+++ src/contexts/QueryContext.jsx
@@ -4,7 +4,6 @@ export const QueryContext = createContext([])
 
 export const QCProvider = (props) => {
     const [additions, setAdditions] = useState(false)
-    const [order, setOrder] = useState('ordering=-')
     const [page, setPage] = useState(1)
     const [pageSize, setPageSize] = useState('page_size=20')
     const [platforms, setPlatforms] = useState('')
@@ -14,17 +13,15 @@ export const QCProvider = (props) => {
     const [searchBar, setSearchBar] = useState('')
     const [sort, setSort] = useState('added')
     
-    const [query, setQuery] = useState('https://api.rawg.io/api/games?key=hellorobots&stores=1&page=' + page + '&' + pageSize + '&' + order + sort + '&exclude_additions=' + additions)
+    const [query, setQuery] = useState('https://api.rawg.io/api/games?key=hellorobots&stores=1&page=' + page + '&' + pageSize + '&ordering=-' + sort + '&exclude_additions=' + additions)
     
     useEffect(() => {
-        setQuery('https://api.rawg.io/api/games?key=hellorobots&stores=1&page=' + page + '&' + pageSize + '&' + order + sort + '&exclude_additions=' + additions + (queryTag != '' ? ('&tags=' + queryTag) : '') + (queryGenre != '' ? ('&genres=' + queryGenre) : '') + (platforms.length != 0 ? ('&platforms=' + platforms) : '') +(search != '' ? ('&search=' + search) : ''))
-    }, [additions, order, page, platforms, queryGenre, queryTag, search, sort])
+        setQuery('https://api.rawg.io/api/games?key=hellorobots&stores=1&page=' + page + '&' + pageSize + '&ordering=-' + sort + '&exclude_additions=' + additions + (queryTag != '' ? ('&tags=' + queryTag) : '') + (queryGenre != '' ? ('&genres=' + queryGenre) : '') + (platforms.length != 0 ? ('&platforms=' + platforms) : '') +(search != '' ? ('&search=' + search) : ''))
+    }, [additions, page, platforms, queryGenre, queryTag, search, sort])
 
     const ccValue = {
         additions,
         setAdditions,
-        order,
-        setOrder,
         page,
         setPage,
         pageSize,
