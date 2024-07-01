74540a3a7fb267c7fea9bf25c1d542f6ef5aff68
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-07-01 00:59:00 -0400

    Added the query client provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './components/Router'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <Router />
-  //</React.StrictMode>
+  </QueryClientProvider>
 )
