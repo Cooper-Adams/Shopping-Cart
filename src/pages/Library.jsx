21f6b69821768d52a750629de64f00d6370e073a
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-07-03 21:58:27 -0400

    Updated library to display the users fake purchased games

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import { LibraryContext } from '../contexts/LibraryContext'
 import Header from '../components/Header'
+import ItemCard from '../components/ItemCard'
+import React, { useContext } from 'react'
+import '../styles/Library.css'
 
 const Library = () => {
+    const { library } = useContext(LibraryContext)
+
     return (
         <>
             <Header />
 
             <div className='library-cont'>
-                <h1>HELLO</h1>
+                <h1 className='library-header'>My Library</h1>
+
+                <div className='pg-container'>
+                    <div className='product-grid'>
+                        {library.map((game) => {
+                            return (
+                                <ItemCard
+                                    game = {game}
+                                    key = {game.id}
+                                />
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
         </>
     )
