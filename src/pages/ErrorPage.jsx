bd6a1dd660d7253050a289f8df1ad3896af717ba
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-07-06 16:08:11 -0400

    Updated the error page messaging

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom'
+import '../styles/ErrorPage.css'
 
-const ErrorPage = () => {
+const ErrorPage = (props) => {
   return (
     <div className='error-div'>
-      <h1 className='error-title'>Oh no, this page does not exist!</h1>
-      <Link to='/'>
+      <h1 className='error-title'>An error has occured!</h1>
+
+      <h3 className='error-title'>We are hard at work to fix it!</h3>
+
+      <Link className='error-link' to='/'>
         You can go back to the home page by clicking here, though!
       </Link>
     </div>
