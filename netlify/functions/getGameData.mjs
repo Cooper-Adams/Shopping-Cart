3109284c2984f4e1989a992dfd60b92ecbd0b425
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-07-07 21:24:10 -0400

    Added a netlify function to get a singular game's data

---
+++ netlify/functions/getGameData.mjs
@@ -0,0 +1,20 @@
+const apiKey = process.env.RAK_ETERNAL
+
+exports.handler = async (event, context) => {
+  try {
+    let gameData = await (await fetch('https://api.rawg.io/api/games/' + event.queryStringParameters.name + '?key=' + apiKey)).json()
+
+    return {
+      statusCode: 200,
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(gameData)
+    }
+  } catch (err) {
+    console.error(err)
+    
+    return {
+      statusCode: 500,
+      message: err
+    }
+  }
+}
