6ef4301b5718f802570c830d29f588749ed055b1
Cooper Adams <cooperadamsbusiness@gmail.com>
2024-07-09 13:45:32 -0400

    Added missing event query parameter for search bar inputs

--- netlify/functions/getResults.mjs
+++ netlify/functions/getResults.mjs
@@ -2,7 +2,7 @@ const apiKey = process.env.RAK_ETERNAL
 
 exports.handler = async (event, context) => {
   try {
-    let gameResults = await (await fetch(event.queryStringParameters.query + '&page=' + event.queryStringParameters.page + '&page_size=' + event.queryStringParameters.page_size + '&ordering=' + event.queryStringParameters.ordering + '&exclude_additions=' + event.queryStringParameters.exclude_additions + (event.queryStringParameters.tags != undefined ? '&tags=' + event.queryStringParameters.tags : '') + (event.queryStringParameters.genres != undefined ? '&genres=' + event.queryStringParameters.genres : '') + (event.queryStringParameters.platforms != undefined ? '&platforms=' + event.queryStringParameters.platforms : '') + '&key=' + apiKey)).json()
+    let gameResults = await (await fetch(event.queryStringParameters.query + '&page=' + event.queryStringParameters.page + '&page_size=' + event.queryStringParameters.page_size + '&ordering=' + event.queryStringParameters.ordering + '&exclude_additions=' + event.queryStringParameters.exclude_additions + (event.queryStringParameters.tags != undefined ? '&tags=' + event.queryStringParameters.tags : '') + (event.queryStringParameters.genres != undefined ? '&genres=' + event.queryStringParameters.genres : '') + (event.queryStringParameters.platforms != undefined ? '&platforms=' + event.queryStringParameters.platforms : '') + (event.queryStringParameters.search != undefined ? '&search=' + event.queryStringParameters.search : '') + '&key=' + apiKey)).json()
 
     return {
       statusCode: 200,
